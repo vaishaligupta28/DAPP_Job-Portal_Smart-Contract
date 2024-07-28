663b89e581db2a42a78b9d0f4320ed46c4e0d249
Vaishali Gupta <g28vaishali@gmail.com>
2024-07-28 18:27:29 +0530

    Setup a basic skeleton react app connecting to smart contracts

---
+++ src/utils/constant.js
@@ -0,0 +1,48 @@
+export const address = "0x55fe86a11dff40b7f5453d67e6dccc15554befee";
+
+export const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_data",
+				"type": "uint256"
+			}
+		],
+		"name": "setData",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "data",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getData",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
